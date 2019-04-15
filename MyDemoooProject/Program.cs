7579046e69d666de167097955e4197191fa9e169
Shivam Gautam <shivam.gautam1026@outlook.com>
2019-04-15 15:14:07 +0530

    A new Hello world pro

---
+++ MyDemoooProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyDemoooProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
